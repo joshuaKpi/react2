bc8d3fb9bb68e04714d9bf52fa4656b77716f3c4
V.Zinkevichius <joshua.kpi@gmail.com>
2016-11-12 19:33:59 +0200

    Editor component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import NoteEditor from './NoteEditor.jsx';
+import NotesGrid from './NotesGrid.jsx';
+
+import './App.less';
 
 const App = React.createClass({
+  handleNoteAdd(data){
+    console.log(data);
+  },
+
   render() {
-    return <h1>Ku-ku</h1>
+    return (
+        <div className='App'>
+          <h2 className='App__header'>NotesApp</h2>
+          <NoteEditor onNoteAdd={this.handleNoteAdd} />
+          <NotesGrid />
+        </div>
+    );
   }
 });
 
---
+++ client/components/NotesGrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotesGrid = React.createClass({
+  render() {
+    return <h1>Notes Grid</h1>
+  }
+});
+
+export default NotesGrid;
